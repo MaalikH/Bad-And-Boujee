21bb335d30b4db7c7d6f9264bf6385d1c7df805e
Maalik <maalik.hornbuckle@bison.howard.edu>
2017-01-18 16:45:42 -0500

    Initial Commit

---
+++ BAD AND BOUJEETests/BAD_AND_BOUJEETests.swift
@@ -0,0 +1,36 @@
+//
+//  BAD_AND_BOUJEETests.swift
+//  BAD AND BOUJEETests
+//
+//  Created by Maalik Hornbuckle on 1/18/17.
+//  Copyright © 2017 Maalik Hornbuckle. All rights reserved.
+//
+
+import XCTest
+@testable import BAD_AND_BOUJEE
+
+class BAD_AND_BOUJEETests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
